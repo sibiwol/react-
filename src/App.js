30c9b7262b2fc28bb90a76e85279bc04bad35068
sibiwol <thinkerring2020@gmail.com>
2021-09-21 15:42:42 +0900

    리액트 생활코딩 강의 class 컴포넌트 이용

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+import "./App.css";
+import TOC from "./components/TOC";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
+import Subject from "./components/Subject";
+import Control from "./components/Control";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.max_content_id = 3;
+    this.state = {
+      mode: "welcome",
+      selected_content_id: 1,
+      subject: { title: "WEB", sub: "World Wide Web" },
+      wlecome: { title: "Welcome", desc: "Hello, React!!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is HyeperText ..." },
+        { id: 2, title: "CSS", desc: "CSS is for design ..." },
+        {
+          id: 3,
+          title: "JavaScript",
+          desc: "Javascript is for interactive ...",
+        },
+      ],
+    };
+  }
+  getReadContent() {
+    let i = 0;
+    while (i < this.state.contents.length) {
+      let data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+      }
+      i = i + 1;
+    }
+  }
+  getContent() {
+    let _title = null;
+    let _desc = null;
+    let _article = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.wlecome.title;
+      _desc = this.state.wlecome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "read") {
+      // filter 사용하면 더 쉽습니다
+      let _content = this.getReadContent();
+      _article = (
+        <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+      );
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            this.max_content_id = this.max_content_id + 1;
+            // let _contents = this.state.contents.concat({
+            //   id: this.max_content_id,
+            //   title: _title,
+            //   desc: _desc,
+            // });
+            let newContents = Array.from(this.state.contents);
+            newContents.push({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: newContents,
+              mode: "read",
+              selected_content_id: this.max_content_id,
+            });
+          }.bind(this)}
+        ></CreateContent>
+      );
+    } else if (this.state.mode === "update") {
+      let _content = this.getReadContent();
+
+      _article = (
+        <UpdateContent
+          data={_content}
+          onSubmit={function (_id, _title, _desc) {
+            let _contents = Array.from(this.state.contents);
+            let i = 0;
+            while (i < _contents.length) {
+              if (_contents[i].id === _id) {
+                _contents[i] = { id: _id, title: _title, desc: _desc };
+                break;
+              }
+              i = i + 1;
+            }
+            this.setState({ contents: _contents, mode: "read" });
+          }.bind(this)}
+        ></UpdateContent>
+      );
+    }
+    return _article;
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
+        ></Subject>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Control
+          onChangeMode={function (_mode) {
+            if (_mode === "delete") {
+              if (window.confirm("really?")) {
+                //누구를 삭제할 것인가
+                let _contents = Array.from(this.state.contents);
+                let i = 0;
+                while (i < _contents.length) {
+                  if (_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+
+                  i = i + 1;
+                }
+                this.setState({
+                  mode: "welcome",
+                  contents: _contents,
+                });
+                alert("delete!");
+              }
+            } else {
+              this.setState({ mode: _mode });
+            }
+          }.bind(this)}
+        ></Control>
+        {this.getContent()}
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return <div className="App">hello react!!</div>;
+// }
+
+export default App;
